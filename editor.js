5971b5a2e7a7941dc9efed96852693ab5f79f231
remsdu64 <rems64des@gmail.com>
2019-09-08 11:34:27 +0200

    Troisieme 1/2 heure

--- editor.js
+++ editor.js
@@ -10,18 +10,67 @@ function getSelectedText() {
 
   return t;
 }
+ipcRenderer.on('save', function() {
+  save();
+});
 
-ipcRenderer.on('file-save', function() {
+ipcRenderer.on('bold', function() {
+  bold();
+});
+
+ipcRenderer.on('italic', function() {
+  italic();
+});
+
+ipcRenderer.on('underline', function() {
+  italic();
+});
+
+
+function save() {
     console.log('save file!!!');
     var selection = getSelectedText();
+    console.log(selection);
     var selection_text = selection.toString();
+    console.log(selection_text);
 
     // How do I add a span around the selected text?
 
-    var span = document.createElement('B');
+    var span = document.createElement('b');
     span.textContent = selection_text;
 
     var range = selection.getRangeAt(0);
     range.deleteContents();
     range.insertNode(span);
-});
+};
+
+
+function italic() {
+    console.log('italic');
+    var selection = getSelectedText();
+    var selection_text = selection.toString();
+
+    var span = document.createElement('i');
+    span.textContent = selection_text;
+
+    var range = selection.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(span);
+};
+
+function bold() {
+    console.log('bold');
+    var selection = getSelectedText();
+    console.log(selection);
+    var selection_text = selection.toString();
+    console.log(selection_text);
+
+    // How do I add a span around the selected text?
+
+    var span = document.createElement('b');
+    span.textContent = selection_text;
+
+    var range = selection.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(span);
+};
